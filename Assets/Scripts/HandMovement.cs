c71dc250a50eaad0a1e863b321be0c62305da42a
Dariusz <dar.marchewka@gmail.com>
2025-07-08 21:26:41 +0200

    Added Game Events example

--- Assets/Scripts/HandMovement.cs
+++ Assets/Scripts/HandMovement.cs
@@ -2,23 +2,40 @@ using UnityEngine;
 
 public class HandMovement : MonoBehaviour
 {
+    [SerializeField] private Transform handTransform;
+    [SerializeField] private float handRangeMax = 1f;
+    [SerializeField] private float handRangeMin = 0.1f;
+
     Camera mainCamera;
     Vector3 mousePos;
 
-
-    // Update is called once per frame
     void Update()
     {
         Vector3 mouseScreenPos = Input.mousePosition;
         mouseScreenPos.z = Vector3.Distance(Camera.main.transform.position, transform.position);
 
         Vector3 worldTarget = Camera.main.ScreenToWorldPoint(mouseScreenPos);
-
-        // point arm toward it
         transform.LookAt(worldTarget);
 
-        // or if it’s just a tip that moves freely:
-        //blueTipTransform.position = worldTarget;
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            float scrollAmount = Input.mouseScrollDelta.y * 0.1f;
+            handTransform.transform.localPosition += transform.forward * scrollAmount;
 
+            float handZPos = handTransform.transform.localPosition.z;
+            if (handZPos >= handRangeMax)
+            {
+                handZPos = handRangeMax;
+            }
+            else if (handZPos <= handRangeMin)
+            {
+                handZPos = handRangeMin;
+            }
+
+            handTransform.transform.localPosition =
+    new Vector3(0f, 0f, handZPos);
+
+
+        }
     }
 }
