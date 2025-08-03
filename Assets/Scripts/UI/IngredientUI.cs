24f12af30ee22bb2ae0b5613f69cc4df509797af
Dariusz <dar.marchewka@gmail.com>
2025-08-03 15:32:26 +0200

    Adding pices to recipe system

--- Assets/Scripts/UI/IngredientUI.cs
+++ Assets/Scripts/UI/IngredientUI.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,9 +7,16 @@ public class IngredientUI : MonoBehaviour
 {
     [SerializeField] private Image icon;
     [SerializeField] private TMP_Text ingredientName;
-    public void Initialize(IngredientData ingredient)
+    public void Initialize(IngredientData ingredient, int pices)
     {
         ingredientName.text = ingredient.ingredientName;
         icon.sprite = ingredient.icon;
+
+        if (pices > 1)
+        {
+            ingredientName.text = $"{ingredient.ingredientName} cut in {pices} pices";
+        }
+
+
     }
 }
