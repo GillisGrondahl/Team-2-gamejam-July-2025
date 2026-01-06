98124af43aa09b04a2809928064ab8ec63bf5305
GeorgHolzapfel <user@SCHORSCH-MOBILT>
2026-01-06 20:55:34 +0100

    changes to IAudioService and FMODAudioManager followin fadein/fadeout changes

--- Assets/Scripts/Interfaces/IAudioService.cs
+++ Assets/Scripts/Interfaces/IAudioService.cs
@@ -5,8 +5,8 @@ public interface IAudioService
     float AmbVolume { get; set; }
     float SfxVolume { get; set; }
     void SetParameter(TrackChannel channel, string paramName, float newValue);
-    void StartTrack(AudioTrackData track, float fadeOutSeconds = 0.5f, float fadeInSeconds = 0.5f);
-    void StopTrack(TrackChannel channel, float fadeOutSeconds = 0f);
+    void StartTrack(AudioTrackData track, bool fadeOut = true, float fadeInSeconds = 0.5f);
+    void StopTrack(TrackChannel channel, bool fadeOut = true);
     void ResetAudio();
     
 }
